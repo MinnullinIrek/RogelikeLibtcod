439483ac1d5849157decbf40b089874bfb8b1027
irek.minnullin <irek.minnullin@theportalsoftware.com>
2023-06-03 11:16:18 +0300

    test

---
+++ src/header.h
@@ -0,0 +1,1 @@
+bool retTrue(bool b = true) { return b; }
--- test/example.cpp
+++ test/example.cpp
@@ -1,5 +1,6 @@
+#include "../src/header.h"
 #include "gtest/gtest.h"
-// #include "../src/header.h"
+
 
 // TEST(cpp_sorter_test, null_term_str_sort) { EXPECT_EQ(retTrue(), true); }
-TEST(cpp_sorter_test, null_term_str_sort) { EXPECT_EQ(1 == 1, true); }
+TEST(cpp_sorter_test, null_term_str_sort) { EXPECT_EQ(retTrue(), true); }
