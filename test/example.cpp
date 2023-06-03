d2cde135f853349e83d4b86a0dbeee9c1f6c91a1
irek.minnullin <irek.minnullin@theportalsoftware.com>
2023-06-03 11:06:52 +0300

    tests

---
+++ test/example.cpp
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+// #include "../src/header.h"
+
+// TEST(cpp_sorter_test, null_term_str_sort) { EXPECT_EQ(retTrue(), true); }
+TEST(cpp_sorter_test, null_term_str_sort) { EXPECT_EQ(1 == 1, true); }
