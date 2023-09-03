79155193e4ea246f8121b21d64bc01aea02cee14
irek.minnullin <irek.minnullin@theportalsoftware.com>
2023-09-03 14:08:39 +0300

    read consts from json

---
+++ src/utils/consts_reader.cpp
@@ -0,0 +1,33 @@
+#include "consts_reader.h"
+
+#include <fstream>
+#include <iostream>
+
+#include "../../json/single_include/nlohmann/json.hpp"
+
+MagicConsts::MagicConsts() {}
+
+void MagicConsts::readJson() {
+  std::string fName = "../resources/const.json";
+  std::ifstream fileStream(fName);
+  if (!fileStream.is_open()) {
+    fName = "resources/const.json";
+    fileStream.open(fName);
+  }
+
+  if (fileStream.is_open()) {
+    nlohmann::json jsonMain1;
+    try {
+      fileStream >> jsonMain1;
+    } catch (...) {
+      std::cerr << "exception on reading " << fName << std::endl;
+    }
+    for (auto& iter = jsonMain1.begin(); iter != jsonMain1.end(); ++iter) {
+      m_consts[iter.key()] = iter.value().get<float>();
+    }
+  } else {
+    throw "file not open fName = " + fName;
+  }
+}
+
+float MagicConsts::getValue(std::string&& name) { return m_consts.at(name); }
---
+++ src/utils/consts_reader.h
@@ -0,0 +1,23 @@
+#ifndef CONSTS_READER_H
+#define CONSTS_READER_H
+
+#include <string>
+#include <unordered_map>
+
+class MagicConsts {
+ public:
+  static MagicConsts& instance() {
+    static MagicConsts magicConsts;
+    return magicConsts;
+  }
+  void readJson();
+  float getValue(std::string&& name);
+
+ private:
+  MagicConsts();
+  std::unordered_map<std::string, float> m_consts;
+};
+
+#define SF MagicConsts::instance().getValue
+
+#endif
