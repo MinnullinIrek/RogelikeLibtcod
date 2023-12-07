6db15839f3e46de604e4165dc250b68fcf0e7f61
irek.minnullin <irek.minnullin@theportalsoftware.com>
2023-12-07 16:29:27 +0300

    log buffer

--- src/utils/event_logger.cpp
+++ src/utils/event_logger.cpp
@@ -1,26 +1,43 @@
 #include "event_logger.h"
 
 #include <cstdarg>
+#include <filesystem>
 #include <format>
 #include <string>
 
-EventLogger::EventLogger(std::string&& fileName) : m_fileName(fileName) {
-  // todo create or check file existing
+EventLogger::EventLogger(std::string&& fileName) : m_fileName(fileName), m_exit(false) {
+  m_file.open("test.txt", std::ios::out);
+  m_writingThread = std::thread(&EventLogger::write, this);
 }
 
-EventLogger::~EventLogger() {}
+EventLogger::~EventLogger() { m_file.close(); }
+
+void EventLogger::write() {
+  while (true) {
+    if (!m_messages.empty()) {
+      const auto& message = m_messages.front();
+      m_file << message.source << ":" << message.text << "\n";
+      m_messages.pop_front();
+    } else {
+      if (m_exit) {
+        std::unique_lock<std::mutex> lk(m_mutex);
+        m_cv.wait(lk);
+      }
+    }
+  }
+}
 
 // void EventLogger::log(std::string_view source, std::string_view format, ...) {
-  // std::format(format, ...);
+// std::format(format, ...);
 
-  // char* sMessage = nullptr;
-  // size_t nLength;
-  // va_list args;
-  // va_start(args, form);
-  // nLength = _vscprintf(format, args) + 1;
-  // sMessage = new char[nLength];
-  // vsprintf_s(sMessage, nLength, format, args);
-  // std::string text(sMessage);
-  // delete[] sMessage;
+// char* sMessage = nullptr;
+// size_t nLength;
+// va_list args;
+// va_start(args, form);
+// nLength = _vscprintf(format, args) + 1;
+// sMessage = new char[nLength];
+// vsprintf_s(sMessage, nLength, format, args);
+// std::string text(sMessage);
+// delete[] sMessage;
 
 // };
--- src/utils/event_logger.h
+++ src/utils/event_logger.h
@@ -1,9 +1,13 @@
 #ifndef EVENT_LOGGER_H
 #define EVENT_LOGGER_H
 
+#include <condition_variable>
 #include <format>
+#include <fstream>
 #include <list>
 #include <string_view>
+#include <thread>
+
 
 struct Message {
   std::string source;
@@ -16,12 +20,20 @@ class EventLogger {
   ~EventLogger();
   template <typename Arg, typename... Args>
   void log(std::string_view source, const Arg& arg, const Args&... args) {
-    auto str = std::format(arg, args...);
+    m_messages.push_back({source, std::format(arg, args...)}) cv.notify_one();
   }
 
+  void write();
+  void turn_off();
+
  private:
+  std::thread m_writingThread;
+  std::condition_variable m_cv;
   std::string m_fileName;
   std::list<Message> m_messages;
+  std::fstream m_file;
+  std::mutex m_mutex;
+  bool m_exit;
 };
 
 #endif
